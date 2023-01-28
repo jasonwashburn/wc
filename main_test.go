e97c859f16c4869b66831511c6a28ac5ec1f1c0e
Jason Washburn <jason@shyftsolutions.io>
2023-01-27 19:54:16 -0600

    adds count bytes flag

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ func TestCountWords(t *testing.T) {
     b := bytes.NewBufferString("word1 word2 word3 word4\n")
     exp := 4
     
-    res := count(b, false)
+    res := count(b, false, false)
 
     if res != exp {
         t.Errorf("Expected %d, got %d instead. \n", exp, res)
@@ -20,7 +20,18 @@ func TestCountLines(t *testing.T) {
     b := bytes.NewBufferString("line1\nline2\nline3\n")
     exp := 3
 
-    res := count(b, true)
+    res := count(b, true, false)
+
+    if res != exp {
+        t.Errorf("Expected %d, got %d instead. \n", exp, res)
+    }
+}
+
+func TestCountBytes(t *testing.T) {
+    b := bytes.NewBufferString("line1\nline2\nline3\n")
+    exp := 18
+
+    res := count(b, false, true)
 
     if res != exp {
         t.Errorf("Expected %d, got %d instead. \n", exp, res)
